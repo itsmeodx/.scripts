37692f68a3d1cc5c6cb54df75485d8d04cf68180
itsmeodx <oussama15ouaadich@gmail.com>
2025-02-23 19:11:55 +0000

    Enhance toggle layout script to preserve existing custom keybindings

--- toggle_layout.sh
+++ toggle_layout.sh
@@ -6,10 +6,23 @@ if ! pgrep -x "gnome-shell" > /dev/null; then
     exit 1
 fi
 
+# Get existing custom keybindings
+CURRENT_BINDINGS=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings)
+# Remove the brackets from the current bindings string
+CURRENT_BINDINGS=${CURRENT_BINDINGS:1:-1}
+
+# Add our new binding path
+NEW_BINDING="'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/'"
+if [[ -z "$CURRENT_BINDINGS" ]]; then
+    FINAL_BINDINGS="[$NEW_BINDING]"
+else
+    FINAL_BINDINGS="[$CURRENT_BINDINGS, $NEW_BINDING]"
+fi
+
 # Set Layout shortcuts
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Toggle Layout'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "sh -c 'layout=\$(setxkbmap -query | grep layout | cut -d: -f2 | tr -d \" \"); if [ \"\$layout\" = \"us\" ]; then setxkbmap ar; else setxkbmap us; fi'"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Control><Alt>c'
 
-# Enable the custom keybinding
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+# Enable custom keybindings while preserving existing ones
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "$FINAL_BINDINGS"
