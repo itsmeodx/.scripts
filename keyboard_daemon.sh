41b81a861c27d00b71f24306297bec4b32496c3f
itsmeodx <oussama15ouaadich@gmail.com>
2025-03-03 23:01:17 +0000

    Change keyboard layout to US when ft_lock is active and layout is not US

--- keyboard_daemon.sh
+++ keyboard_daemon.sh
@@ -6,7 +6,7 @@ while true; do
 	LAYOUT=$(setxkbmap -query | awk '/layout:/ {print $2}')
 
 	# Check if ft_lock is running and the layout is Arabic
-	if pgrep -x "ft_lock" > /dev/null && [ "$LAYOUT" = "ar" ]; then
+	if pgrep -x "ft_lock" > /dev/null && [ "$LAYOUT" != "us" ]; then
 		setxkbmap us
 		# Send a notification
 		notify-send "Keyboard Layout" "ft_lock is running, keyboard layout changed to US" -u normal -i keyboard
