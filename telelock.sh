68b68a422efcba81f05320987e2613f06492fd49
itsmeodx <oussama15ouaadich@gmail.com>
2025-03-11 15:06:19 +0000

    Add telelock script to manage session locking based on phone connection

---
+++ telelock.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+# Export your phone's ID (via lsusb) in MYPHONEID in your .zshrc
+source ~/.zshrc
+
+# Run as a daemon
+while true; do
+	# Check if the device is connected
+	if lsusb | grep -q $MYPHONEID; then
+		if [ ! -f /tmp/telelock ]; then
+			touch /tmp/telelock
+			pkill -KILL ft_lock
+			notify-send "Session unlocked" "ft_lock has been killed" -u normal -i lock
+		fi
+	else
+		[ -f /tmp/telelock ] && rm /tmp/telelock
+	fi
+
+	# Wait before checking again
+	sleep 2
+done &
