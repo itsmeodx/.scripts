0ecd396480bb37c7072cf61b0f59e9b8fc6682c3
itsmeodx <oussama15ouaadich@gmail.com>
2025-03-12 08:14:20 +0000

    Add PATH variable check and prompt for bin directory addition in installation scripts

--- neovim.sh
+++ neovim.sh
@@ -49,7 +49,18 @@ mkdir -p $bin_dir
 ln -sf $dest/bin/nvim $bin_dir/nvim
 
 # Checking the PATH variable
-./checking_PATH.sh
+echo "Checking if the bin directory is in the PATH variable..."
+if [[ ":$PATH:" != *":$bin_dir:"* ]]; then
+	echo "Warning: $bin_dir is not in your PATH"
+	echo "Would you like to add it to your PATH? [y/N]"
+	read add_to_path
+	if [[ $add_to_path =~ ^[Yy]$ ]]; then
+		echo "export PATH=\$PATH:$bin_dir" >> $HOME/.zshrc
+		echo "PATH variable updated. Please restart your shell to apply the changes."
+	fi
+else
+	echo "The bin directory is in your PATH"
+fi
 
 # Clean up
 echo "Cleaning up..."
